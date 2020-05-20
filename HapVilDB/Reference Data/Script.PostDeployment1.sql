8335324f69c6ba2065d23042e8774f571926666f
Brigitte Hennequin <4_sAfJO25N@outlook.com>
2020-05-20 18:14:49 +0200

    Adjust DatabaseTable with new sql script

--- HapVilDB/Reference Data/Script.PostDeployment1.sql
+++ HapVilDB/Reference Data/Script.PostDeployment1.sql
@@ -9,5 +9,5 @@ Post-Deployment Script Template
                SELECT * FROM [$(TableName)]					
 --------------------------------------------------------------------------------------
 */
- :r .\Images.data.sql	
+ 
  GO
--- HapVilDB/dbo/Tables/createImages.sql
+++ HapVilDB/dbo/Tables/createImages.sql
@@ -1,5 +1,12 @@
 ﻿CREATE TABLE [dbo].[Images]
 (
 	[Id] INT NOT NULL PRIMARY KEY IDENTITY, 
-    [Title] VARCHAR(50) NOT NULL
+    [Title] VARCHAR(50) NOT NULL, 
+    [Image] NVARCHAR(50) NULL, 
+    [Description] VARCHAR(50) NULL, 
+    [Format] NVARCHAR(50) NULL, 
+    [Subject] INT NOT NULL, 
+    [User] INT NOT NULL, 
+    [Date] DATE NULL
+
 )
